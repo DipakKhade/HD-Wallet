3572c2c77695657352f31b554af488e6d523ac6a
= <=>
2024-08-18 23:09:38 +0530

    generating mnemonic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import Appbar from "@/components/Appbar";
+import { Card } from "@/components/Card";
+import Hero from "@/components/Hero";
+import SecretRecoveryPhrase from "@/components/Phrase";
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className="flex flex-col justify-center">
+      <Appbar/>
+      <Hero/>
+     <SecretRecoveryPhrase/>
     </main>
   );
 }

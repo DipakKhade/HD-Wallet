4ac649c02217d3dd90f6041dd7d336aaf430e97a
= <=>
2024-08-18 23:38:00 +0530

    copying mnemonic

--- src/components/Phrase.tsx
+++ src/components/Phrase.tsx
@@ -1,45 +1,65 @@
-'use client';
+"use client";
 import { generateMnemonic, mnemonicToSeedSync } from "bip39";
 import { Button } from "./ui/button";
 import { useState } from "react";
 import { Card } from "./Card";
+import { FaRegCopy } from "react-icons/fa6";
 
 export default function SecretRecoveryPhrase() {
+  const [mnemonic, SetMnemonic] = useState<string>();
 
-const [mnemonic , SetMnemonic ]= useState<string>()
+  //   const seed = mnemonicToSeedSync(mnemonic);
 
-//   const seed = mnemonicToSeedSync(mnemonic);
-
-
-
-  return <>
-
-  <div className="m-auto p-4">
-    <Button
-    onClick={()=>{
-        SetMnemonic(generateMnemonic())
-    }}
-    >Generate Secrat Reacovery Phrase</Button>
-
-    {
-       mnemonic ? <Mnemonic mnemonic={mnemonic.split(' ')}/> :''
-    }
-  </div>
-  </>;
+  return (
+    <>
+      <div className="p-4 flex flex-col justify-center w-96 m-auto">
+        <Button
+          onClick={() => {
+            SetMnemonic(generateMnemonic());
+          }}
+        >
+          Generate Secrat Reacovery Phrase
+        </Button>
+      </div>
+      {mnemonic ? (
+          <Mnemonic mnemonic={mnemonic.split(" ")} mnemonicstr={mnemonic} />
+        ) : (
+          ""
+        )}
+    </>
+  );
 }
 
-
-function Mnemonic({mnemonic}:{mnemonic:string[]}){
-    return<>
-    <Card>
-        
-    {mnemonic.map((word:string,index:number)=>(
-        <div key={index}>
-            <span>{index}</span> {word}
+function Mnemonic({
+  mnemonic,
+  mnemonicstr,
+}: {
+  mnemonic: string[];
+  mnemonicstr: string;
+}) {
+  return (
+    <>
+      <Card>
+        <div className="grid grid-cols-3 gap-4">
+          {mnemonic.map((word: string, index: number) => (
+            <div key={index} className="flex items-center text-md">
+              <span className="pr-2">{index + 1} </span>
+              <div className="font-semibold">{word}</div>
+            </div>
+          ))}
+          <div className="pt-6">
+            <button
+            className="flex "
+              onClick={async () => {
+                await navigator.clipboard.writeText(mnemonicstr);
+              }}
+            >
+              <FaRegCopy />
+              <span className="pb-6 pl-2">copy mnemonic</span>
+            </button>
+          </div>
         </div>
-    ))}
-    
-    </Card>
+      </Card>
     </>
-
+  );
 }
